de17258956c58bb7fb10fe6bb704fb6cd2733b47
pjk0007 <44570338+pjk0007@users.noreply.github.com>
2021-12-20 13:48:47 +0900

    added box scale animation(hover) and used transform-origin on first and last child of boxes

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -58,6 +58,12 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-image: url(${(props) => props.bgPhoto});
   background-size: cover;
   background-position: center center;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
 
 const rowVariants = {
@@ -69,6 +75,22 @@ const rowVariants = {
   },
   exit: {
     x: -window.outerWidth + 10,
+  },
+};
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+
+  hover: {
+    scale: 1.5,
+    y: -50,
+    transition: {
+      delay: 0.5,
+      duration: 0.3,
+      type: "tween",
+    },
   },
 };
 
@@ -122,6 +144,10 @@ function Home() {
                   .map((movie) => (
                     <Box
                       key={movie.id}
+                      whileHover="hover"
+                      initial="normal"
+                      variants={boxVariants}
+                      transition={{ type: "tween" }}
                       bgPhoto={makeImagePath(movie.backdrop_path ?? "", "w500")}
                     ></Box>
                   ))}
